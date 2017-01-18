fe22291b272dee806fc9eb67d21599a8d8027ed7
Aaron Scherer <aequasi@gmail.com>
2017-01-18 14:03:30 -0800

    Updating message formatter

---
+++ src/Logging/MessageFormatter.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of php-restcord.
+ *
+ * (c) Aaron Scherer <aequasi@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE
+ */
+
+namespace RestCord\Logging;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @author Aaron Scherer <aequasi@gmail.com>
+ *
+ * MessageFormatter Class
+ */
+class MessageFormatter extends \GuzzleHttp\MessageFormatter
+{
+    /**
+     * @var
+     */
+    private $token;
+
+    /**
+     * MessageFormatter constructor.
+     *
+     * @param string $template
+     * @param        $token
+     */
+    public function __construct($template = \GuzzleHttp\MessageFormatter::CLF, $token)
+    {
+        \GuzzleHttp\MessageFormatter::__construct($template);
+        $this->token = $token;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function format(
+        RequestInterface $request,
+        ResponseInterface $response = null,
+        \Exception $error = null
+    ) {
+        $template = parent::format($request, $response, $error);
+
+        return str_replace($this->token, '<TOKEN>', $template);
+    }
+}
