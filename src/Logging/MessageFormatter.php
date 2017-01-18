6679b4fc4be1ee39c671a24e03d0005835e86c9d
Aaron Scherer <aequasi@gmail.com>
2017-01-18 22:03:39 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Logging/MessageFormatter.php
+++ src/Logging/MessageFormatter.php
@@ -1,12 +1,14 @@
 <?php
 
 /*
- * This file is part of php-restcord.
- *
- * (c) Aaron Scherer <aequasi@gmail.com>
+ * Copyright 2017 Aaron Scherer
  *
  * This source file is subject to the license that is bundled
  * with this source code in the file LICENSE
+ *
+ * @package     restcord/restcord
+ * @copyright   Aaron Scherer 2017
+ * @license     MIT
  */
 
 namespace RestCord\Logging;
@@ -32,7 +34,7 @@ class MessageFormatter extends \GuzzleHttp\MessageFormatter
      * @param string $template
      * @param        $token
      */
-    public function __construct($template = \GuzzleHttp\MessageFormatter::CLF, $token)
+    public function __construct($template, $token)
     {
         \GuzzleHttp\MessageFormatter::__construct($template);
         $this->token = $token;
