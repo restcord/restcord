b1feba9189b85558e6b3169bddd74fb26b092f66
Sascha Ohms <sasch9r@gmail.com>
2017-04-20 00:02:03 +0200

    StyleCI fixes

--- src/DiscordClient.php
+++ src/DiscordClient.php
@@ -160,7 +160,7 @@ class DiscordClient
                     if (0 === stripos($value, 'Bearer ')) {
                         $value                = substr($value, 7);
                     }
-                    
+
                     return $value;
                 }
             )
@@ -178,7 +178,7 @@ class DiscordClient
                     } else {
                         $value = '';
                     }
-                    
+
                     return $value;
                 }
             );
