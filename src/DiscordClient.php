8316bcd389a067e1e831dde9ae15b0d164bd9be5
Aaron Scherer <aequasi@gmail.com>
2018-02-01 10:07:41 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/DiscordClient.php
+++ src/DiscordClient.php
@@ -23,9 +23,9 @@ use GuzzleHttp\Middleware;
 use Monolog\Logger;
 use Psr\Http\Message\ResponseInterface;
 use RestCord\Logging\MessageFormatter;
-use RestCord\RateLimit\RateLimiter;
 use RestCord\RateLimit\Provider\AbstractRateLimitProvider;
 use RestCord\RateLimit\Provider\MemoryRateLimitProvider;
+use RestCord\RateLimit\RateLimiter;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use function GuzzleHttp\json_decode;
 
