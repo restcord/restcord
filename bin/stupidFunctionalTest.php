4e75419ed0cb20f11ec1cd131eedfffcdf401a6b
Aaron Scherer <aequasi@gmail.com>
2018-02-15 18:58:34 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- bin/stupidFunctionalTest.php
+++ bin/stupidFunctionalTest.php
@@ -132,52 +132,52 @@ $client->channel->createMessage(
 $client->channel->createMessage(
     [
         'channel.id' => $channels[0]->id,
-        'content'    => "this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js
+        'content'    => 'this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js
  function foo(bar) {
    console.log(bar);
  }
  
- foo(1);```",
+ foo(1);```',
         'embed'      => [
-            "title"       => "title ~~(did you know you can have markdown here too?)~~",
-            "description" => "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
-            "url"         => "https://discordapp.com",
-            "color"       => 14290439,
-            "timestamp"   => "2017-02-20T18:05:58.512Z",
-            "footer"      => [
-                "icon_url" => "https://cdn.discordapp.com/embed/avatars/0.png",
-                "text"     => "footer text"
+            'title'       => 'title ~~(did you know you can have markdown here too?)~~',
+            'description' => "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+            'url'         => 'https://discordapp.com',
+            'color'       => 14290439,
+            'timestamp'   => '2017-02-20T18:05:58.512Z',
+            'footer'      => [
+                'icon_url' => 'https://cdn.discordapp.com/embed/avatars/0.png',
+                'text'     => 'footer text',
             ],
-            "thumbnail"   => [
-                "url" => "https://cdn.discordapp.com/embed/avatars/0.png"
+            'thumbnail'   => [
+                'url' => 'https://cdn.discordapp.com/embed/avatars/0.png',
             ],
-            "image"       => [
-                "url" => "https://cdn.discordapp.com/embed/avatars/0.png"
+            'image'       => [
+                'url' => 'https://cdn.discordapp.com/embed/avatars/0.png',
             ],
-            "author"      => [
-                "name"     => "author name",
-                "url"      => "https://discordapp.com",
-                "icon_url" => "https://cdn.discordapp.com/embed/avatars/0.png"
+            'author'      => [
+                'name'     => 'author name',
+                'url'      => 'https://discordapp.com',
+                'icon_url' => 'https://cdn.discordapp.com/embed/avatars/0.png',
             ],
-            "fields"      => [
+            'fields'      => [
                 [
-                    "name"  => "Foo",
-                    "value" => "some of these properties have certain limits..."
+                    'name'  => 'Foo',
+                    'value' => 'some of these properties have certain limits...',
                 ],
                 [
-                    "name"  => "Bar",
-                    "value" => "try exceeding some of them!"
+                    'name'  => 'Bar',
+                    'value' => 'try exceeding some of them!',
                 ],
                 [
-                    "name"  => " 😃",
-                    "value" => "an informative error should show up, and this view will remain as-is until all issues are fixed"
+                    'name'  => ' 😃',
+                    'value' => 'an informative error should show up, and this view will remain as-is until all issues are fixed',
                 ],
                 [
-                    "name"  => "<:thonkang:219069250692841473>",
-                    "value" => "???"
-                ]
-            ]
-        ]
+                    'name'  => '<:thonkang:219069250692841473>',
+                    'value' => '???',
+                ],
+            ],
+        ],
     ]
 );
 
