3abd3fa5702efb1191401506957bcc268c978b00
Kirihito <support@satowa-network.at>
2022-02-21 18:47:19 +0100

    removed Copyright

--- src/Traits/BannerTrait.php
+++ src/Traits/BannerTrait.php
@@ -1,11 +1,7 @@
 <?php
 ////////////////////////////////////////////////////
-//  Copyright by: Satowa Network                  //
 //  Author: Kirihito                              //
-//  Discord: https://discord.satowa-network.at    //
-//  Email: support@satowa-network.at              //
-//  Website: https://satowa-network.at            //
-////////////////////////////////////////////////////
+///////////////////////////////////////////////////
 
 namespace RestCord\Traits;
 use RestCord\Constants;
