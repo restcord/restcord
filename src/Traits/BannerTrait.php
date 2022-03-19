7d3cbe381f9515c4cdc5bb93c408524de4a647dc
Slush <rology@satowa-network.at>
2022-03-19 14:00:59 +0100

    Revert "removed Copyright"
    This reverts commit 3abd3fa5702efb1191401506957bcc268c978b00.

--- src/Traits/BannerTrait.php
+++ src/Traits/BannerTrait.php
@@ -1,7 +1,11 @@
 <?php
 ////////////////////////////////////////////////////
+//  Copyright by: Satowa Network                  //
 //  Author: Kirihito                              //
-///////////////////////////////////////////////////
+//  Discord: https://discord.satowa-network.at    //
+//  Email: support@satowa-network.at              //
+//  Website: https://satowa-network.at            //
+////////////////////////////////////////////////////
 
 namespace RestCord\Traits;
 use RestCord\Constants;
