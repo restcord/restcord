e6b778e9211a8db0faf6da8fad4f329a38bb8d0f
Slush <rology@satowa-network.at>
2022-03-19 16:55:55 +0100

    Fixed Guild Avatar Uri

--- src/Traits/IconTrait.php
+++ src/Traits/IconTrait.php
@@ -24,7 +24,8 @@ trait IconTrait
 {
     public function getIcon($format = 'webp', $size = null)
     {
-        $url = Constants::AVATAR_URL.$this->id.'/'.$this->icon.'.'.$format;
+        // Add const SERVER_URL = self::CDN_URL.'icons/'; to the Constants.php
+        $url = "https://cdn.discordapp.com/icons/".$this->id.'/'.$this->icon.'.'.$format;
         if ($size !== null) {
             $url .= '?size='.$size;
         }
