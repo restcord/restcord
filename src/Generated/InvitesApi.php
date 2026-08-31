55c7540c055b2ffaff52241bd7bae5e7b9737392
Aaron Scherer <aequasi@gmail.com>
2026-08-30 23:09:31 -0500

    fix(ci): restore canonical generated output
    An older queued StyleCI run formatted files now excluded from its finder.

--- src/Generated/InvitesApi.php
+++ src/Generated/InvitesApi.php
@@ -2,17 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * Copyright 2017 Aaron Scherer
- *
- * This source file is subject to the license that is bundled
- * with this source code in the file LICENSE
- *
- * @package     restcord/restcord
- * @copyright   Aaron Scherer 2017
- * @license     MIT
- */
-
 namespace RestCord\Generated;
 
 use GuzzleHttp\Promise\PromiseInterface;
